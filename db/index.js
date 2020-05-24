8459d571cb7e92ac64ae42250d918fce4869a248
yhafez <yhafez3@gmail.com>
2020-05-24 04:08:19 -0700

    Deployed to Heroku, updated port and client

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@
 
 
 const { Client } = require('pg');
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 
 /*------------------------------------------------------------------------------- Functions -------------------------------------------------------------------------------*/
